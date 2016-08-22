bd8aea0907b3f2bd84ca264f515eae0ff27e0516
zbyszeg <zbyszek.gora@gmail.com>
2016-08-22 15:06:27 +0200

    dodano komentarze Disquis

--- main/ad.php
+++ main/ad.php
@@ -1,3 +1,24 @@
 <h1>Kontroler domeny Active Directory w systemie Debian Linux</h1>
 
 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vitae aliquam sem. Proin sit amet luctus ante. Sed risus arcu, cursus non bibendum sit amet, euismod eget urna. Cras varius dui ac congue condimentum. Nullam nec felis sed augue dignissim tincidunt sed eget nisi. Maecenas convallis, massa sit amet pharetra ultricies, dui mauris suscipit leo, eget varius risus velit sed tortor. Aliquam sit amet lacus sem. Nullam luctus quam sodales finibus vehicula.<br /><br />Praesent lacinia, lorem non eleifend fermentum, lectus est ullamcorper diam, vel feugiat nibh leo ut ex. Duis blandit iaculis nisl vitae ultricies. Phasellus vel purus tortor. Vivamus egestas lorem eu velit egestas sagittis. Aliquam posuere magna faucibus elit iaculis, sed venenatis risus gravida. Etiam elementum ac neque ut imperdiet. Vestibulum velit neque, blandit ut mollis vitae, ultrices et dolor. Quisque at risus a arcu consectetur venenatis eget eget lectus. Donec nec purus eget odio eleifend dapibus. Duis suscipit neque ac velit faucibus, ac ornare nibh ultrices.
+<br/></br><hr/><br/>
+<div id="disqus_thread"></div>
+<script>
+
+/**
+ *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+ *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables */
+/*
+var disqus_config = function () {
+    this.page.url = PAGE_URL;  // Replace PAGE_URL with your page's canonical URL variable
+    this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+};
+*/
+(function() { // DON'T EDIT BELOW THIS LINE
+    var d = document, s = d.createElement('script');
+    s.src = '//samba-org-pl.disqus.com/embed.js';
+    s.setAttribute('data-timestamp', +new Date());
+    (d.head || d.body).appendChild(s);
+})();
+</script>
+<noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
